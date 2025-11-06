2cd56ceff90d91123cc668e746473e5228807d87
Max <max.den.oudsten@gmail.com>
2025-11-06 12:44:52 +0100

    adds comment to code line

--- todo-app/src/main.jsx
+++ todo-app/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render( // This renders the React application into the root div
   <StrictMode>
     <App />
   </StrictMode>,
